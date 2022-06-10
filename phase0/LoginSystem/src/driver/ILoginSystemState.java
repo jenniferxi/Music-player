0140d466a4ac84490f49a110de2df4ccc886f671
Erhan Atesoglu <github@pcmwave.com>
2022-06-10 19:02:06 -0400

    Clean up from JShell to LoginSystem

---
+++ phase0/LoginSystem/src/driver/ILoginSystemState.java
@@ -0,0 +1,36 @@
+package driver;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Stack;
+
+/*import file_system.Directory;
+import file_system.VirtualFileSystem;*/
+
+public interface IShellState extends Serializable {
+    boolean isRunning();
+
+    void stopRunning();
+/*
+    VirtualFileSystem getFileSystem();
+
+    Directory getRootDir();
+
+    Directory getCurrentDir();
+
+    void setCurrentDir(Directory newCurrentDir);
+*/
+    void addHistory(String inputLine);
+
+    void removeHistory(int index);
+
+    List<String> getHistory();
+
+    void addCorrectHistory(String inputLine);
+
+    List<String> getCorrectHistory();
+
+    /*
+    Stack<Directory> getDirectoryStack();*/
+
+}
